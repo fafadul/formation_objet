828b10cccf9218e2f18f68efb3e3a0da13f3ccb3
Fadul <fadul.fatouma@gmail.com>
2016-05-03 11:02:16 +0200

    Method jeje

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 
 public class Main {
-	
-	//Method bonjour()
-	public static void bonjour(){
-		System.out.println(" Bonjour");
+
+	// Method bonjour()
+	public static void bonjour() {
+		System.out.println("Bonjour");
 	}
-	
-	public static void hello(){
-		System.out.println(" Hello");
+
+	public static void hello() {
+		System.out.println("Hello");
 	}
-	
+
+	public static void jeje() {
+		System.out.println("Jéjé");
+	}
+
 	public static void main(String[] args) {
-		
+
 		bonjour();
 		hello();
-		
+		jeje();
+
 	}
 
 }
