74d48ee6fab54f033da943ceaa206dadbb21ab12
Fadul <fadul.fatouma@gmail.com>
2016-05-03 10:29:23 +0200

    New method bonjour

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 
 public class Main {
+	
+	//Method bonjour()
+	public static void bonjour(){
+		System.out.println(" Bonjour");
+	}
+	
+	public static void main(String[] args) {
+		
+		bonjour();
+		
+	}
 
 }
